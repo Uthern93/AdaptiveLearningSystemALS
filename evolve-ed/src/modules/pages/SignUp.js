a7bda7cc241f932f648ea911e04d5c9b7c36a6cc
keabt <keabhello@gmail.com>
2024-05-01 13:55:35 +0100

    mui/joy added

--- evolve-ed/src/modules/pages/SignUp.js
+++ evolve-ed/src/modules/pages/SignUp.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button, Dialog, DialogPanel } from '@tremor/react';
 
 
 function SignUp() {
@@ -8,19 +7,7 @@ function SignUp() {
   return (
     <>
         <div>SignUp</div>
-        <Button className="mx-auto block" onClick={() => setIsOpen(true)}>Open Dialog</Button>
-    <Dialog open={isOpen} onClose={(val) => setIsOpen(val)} static={true}>
-      <DialogPanel>
-        <h3 className="text-lg font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">Account Created Successfully</h3>
-        <p className="mt-2 leading-6 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-          Your account has been created successfully. You can now login to your
-          account. For more information, please contact us.
-        </p>
-        <Button className="mt-8 w-full" onClick={() => setIsOpen(false)}>
-          Got it!
-        </Button>
-      </DialogPanel>
-    </Dialog>
+]
 
     </>
 
