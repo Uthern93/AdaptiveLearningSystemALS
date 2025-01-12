6a4c0581ce1d627d08dcca5eb2975cd9fef2b5a8
Uthern <uthern4@gmail.com>
2025-01-12 16:47:03 +0800

    changes

---
+++ evolve-ed/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './modules/pages/Landing';
+import Login from './modules/pages/Login';
+import SignUp from'./modules/pages/SignUp';
+import Subjects from './modules/pages/Subjects';
+import Algebra from './modules/pages/Algebra';
+import Tutorials from './modules/pages/Tutorials';
+import Dashboard from './modules/pages/Dashboard';
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/subjects" element={<Subjects />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/algebra" element={<Algebra />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ evolve-ed/src/modules/components/SubjectCard.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardActions from '@mui/joy/CardActions';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import Typography from '@mui/joy/Typography';
+import BakeryDiningIcon from '@mui/icons-material/BakeryDining';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import { useNavigate } from 'react-router-dom';
+
+function SubjectCard() {
+  const navigate = useNavigate();
+
+  return (
+    <Card
+    variant="soft"
+    color="neutral"
+  
+        data-resizable
+        sx={{
+          textAlign: 'center',
+          alignItems: 'center',
+          width: 350,
+          height: 350,
+          '--icon-size': '100px',
+        }}
+      >
+        <CardOverflow variant="solid" color="primary">
+          <AspectRatio
+            variant="plain"
+            color="primary"
+            ratio="1"
+            sx={{
+              m: 'auto',
+              transform: 'translateY(50%)',
+              borderRadius: '50%',
+              width: 'var(--icon-size)',
+              boxShadow: 'sm',
+              bgcolor: 'background.surface',
+              position: 'relative',
+            }}
+          >
+            <div>
+              <CalculateIcon color="primary" sx={{ fontSize: '4rem' }} />
+            </div>
+          </AspectRatio>
+        </CardOverflow>
+        <Typography level="title-lg" sx={{ mt: 'calc(var(--icon-size) / 2)' }}>
+          🎊 Algebra foundations 🎊
+        </Typography>
+        <CardContent sx={{ maxWidth: '40ch' }}>
+        Why did humans invent algebra in the first place? 
+        </CardContent>
+        <CardActions
+          orientation="vertical"
+          buttonFlex={1}
+          sx={{
+            '--Button-radius': '40px',
+            width: 'clamp(min(100%, 160px), 50%, min(100%, 200px))',
+          }}
+        >
+          <Button variant="solid" color="primary" onClick={() => navigate("/tutorials")}>
+            Start
+          </Button>
+        </CardActions>
+      </Card>
+  )
+}
+
+export default SubjectCard;
