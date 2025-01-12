e447938b9b1f74fb8bfb8ab509b9128203cf5628
irfanhafizi <irfanhafizi2000@gmail.com>
2025-01-12 23:05:06 +0800

    update

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const User = require('../models/user');
+const Subject = require('../models/subject');
 
 const router = express.Router();
 
 router.get('/users', async (req, res) => {
   // try {
-    const users = await User.findAll();
-    res.json(users);
+    const subject = await Subject.findAll();
+    res.json(subject);
     // res.json('woi');
   // } catch (error) {
   //   res.status(500).json({ error: 'Internal Server Error' });
