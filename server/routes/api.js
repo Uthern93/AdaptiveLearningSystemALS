0717800d6f08ff622116c13da0b0b7f421da3427
Uthern <uthern4@gmail.com>
2025-01-13 10:57:08 +0800

    Reapply "Api successfully running"
    This reverts commit 77f5c9f423892a0e3a98fcf927cd61e24a5b313e.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,17 +1,24 @@
+// routes/api.js
 const express = require('express');
-const Subject = require('../models/subject');
-
-
 const router = express.Router();
+const db = require('../models');
+const Subject = db.Subject;
 
-// app.get("/users", (req, res) => {
-//   Subject.findAll()
-//     .then((subjects) => {
-//       res.send(subjects);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     })
-// })
+// routes/api.js
+router.get("/users", async (req, res) => {
+    console.log('GET /api/users request received');
+    try {
+      const subjects = await Subject.findAll();
+      console.log('Subjects retrieved:', subjects);
+      res.json(subjects);
+    } catch (err) {
+      console.error('Error in GET /api/users:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+  router.get("/test", (req, res) => {
+    res.json({ message: "API is working" });
+  });
 
 module.exports = router;
