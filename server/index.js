77f5c9f423892a0e3a98fcf927cd61e24a5b313e
Uthern <uthern4@gmail.com>
2025-01-13 10:54:04 +0800

    Revert "Api successfully running"
    This reverts commit 75b347bb16c618952af1ba80b3002d82d0cfd89e.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
 const userRoutes = require('./routes/api');
 const db = require('./models');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
+
 app.use(express.json());
-app.use('/api', userRoutes);
+// app.use('/api', userRoutes);
+app.get("/users", (req, res) => {
+  Subject.findAll()
+    .then((subjects) => {
+      res.send(subjects);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
 
 app.listen(port, () => {
-  console.log(`🚀 Backend server is running on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
