75b347bb16c618952af1ba80b3002d82d0cfd89e
Uthern <uthern4@gmail.com>
2025-01-13 10:53:41 +0800

    Api successfully running

--- server/index.js
+++ server/index.js
@@ -1,24 +1,13 @@
 const express = require('express');
-const { Sequelize } = require('sequelize');
 const userRoutes = require('./routes/api');
 const db = require('./models');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
-
+const port = process.env.PORT || 5000;
 app.use(express.json());
-// app.use('/api', userRoutes);
-app.get("/users", (req, res) => {
-  Subject.findAll()
-    .then((subjects) => {
-      res.send(subjects);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`🚀 Backend server is running on port ${port}`);
 });
